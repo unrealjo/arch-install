fe6ae9764ff1cc6da26daf17065a9c58e2a2c793
Youssef Eddaif <63888397+yeddaif@users.noreply.github.com>
2021-12-11 20:07:36 +0100

    Add files via upload

--- i3.sh
+++ i3.sh
@@ -1,8 +1,8 @@
  #!/bin/bash
  # Xorg
-sudo pacman -S --noconfirm  xorg-server xorg-xinit xorg-xrandr xorg-xkill 
+sudo pacman -S --noconfirm linux-zen base-devel linux-zen-headers linux-lts xorg xorg-xinit xorg-xrandr xorg-xkill 
 # Drivers
-sudo pacman -S --noconfirm xf86-input-synaptics xf86-video-intel mesa xf86-input-libinput intel-ucode
+sudo pacman -S --noconfirm intel-ucode
  # Audio and video
 sudo pacman -S --noconfirm  pulseaudio pavucontrol  mpv brightnessctl
  # Appearance
@@ -13,8 +13,6 @@ sudo pacman -S --noconfirm pcmanfm xarchiver unrar unzip p7zip zip
 sudo pacman -S --noconfirm zathura zathura-pdf-mupdf     
 # Additional
 sudo pacman -S --noconfirm rofi libnotify dunst neofetch acpi htop usbutils lxsession
-# Virt manager
-sudo pacman -S --needed qemu virt-manager ebtables dnsmasq
 # Dev
 sudo pacman -S --noconfirm  jdk-openjdk nodejs npm android-tools
 # Terminal
--- paru.sh
+++ paru.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
  cd $HOME/arch-install
  sudo pacman -U --needed ./paru-1.9.0-1-x86_64.pkg.tar.zst
- sleep 2 ;paru -S polybar picom-ibhagwan-git brave-bin teams telegram-desktop-bin 
+ sleep 2 ;paru -S anbox-image-houdini anbox-git polybar picom-ibhagwan-git brave-bin teams telegram-desktop-bin 

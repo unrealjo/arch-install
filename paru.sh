a6b746ffad2a6f6a80079bbc91ead7a7d0d6217e
Youssef Eddaif <yeddaif18@gmail.com>
2021-12-27 18:54:12 +0100

    Fixing reinstallation of paru

--- paru.sh
+++ paru.sh
@@ -1,6 +1,8 @@
  #!/bin/bash
- #git clone https://aur.archlinux.org/paru.git
-#cd paru
-#makepkg -si
-#sleep 2 ;cd;
+if [[ ! -e /bin/paru ]] ; then
+    git clone https://aur.archlinux.org/paru.git
+    cd paru
+    makepkg -si
+    sleep 2 ;cd;
+fi
 paru -S polybar picom-ibhagwan-git brave-bin

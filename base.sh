39b311100b0be1a62219ca0a2892f9a1b08a85cb
Youssef Eddaif <yeddaif18@gmail.com>
2022-01-02 14:43:14 +0100

    adding libnotify package and removing firefox

--- base.sh
+++ base.sh
@@ -9,7 +9,7 @@ locale-gen
 # set keyboard layout
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=fr" >> /etc/vconsole.conf
-echo "Arch" >> /etc/hostname
+echo "arch" >> /etc/hostname
 
 # set local host address
 echo "127.0.0.1 localhost" >> /etc/hosts
@@ -17,15 +17,16 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 Arch.localdomain Arch" >> /etc/hosts
 echo "STEP ON DONE !"
 echo "installing essential packages"
-pacman -S --noconfirm grub iw iwd dhcpcd
+pacman -S --noconfirm grub iw iwd dhcpcd sudo
 systemctl enable iwd dhcpcd
-echo "Adding linus user " && useradd -m -G wheel,storage linus 
+echo "Adding linus user " && useradd -m -G wheel,storage,video linus 
 echo "Installing grub"
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 echo "Last step done"
 echo "Change root password"; passwd
 echo "Change linus password"; passwd linus
-
+sed -i '82s/.//' /etc/sudoers
+echo "Making wheel able to use sudo as root"
 
 
--- i3.sh
+++ i3.sh
@@ -1,24 +1,25 @@
  #!/bin/bash
  # Xorg
-sudo pacman -S --noconfirm --needed base-devel rust xorg-server xorg-xinit xorg-xrandr xorg-xkill intel-ucode
+sudo pacman -S --noconfirm --needed base-devel rust xorg-server xorg-xinit xorg-xrandr xorg-xkill intel-ucode xf86-video-intel
  # Audio and video
-sudo pacman -S --noconfirm --needed  pulseaudio pavucontrol  mpv mpd ncmpcpp mpc brightnessctl lxappearance i3-gaps feh pamixer
+sudo pacman -S --noconfirm --needed  pulseaudio mpv mpd mpc brightnessctl lxappearance i3-gaps feh pamixer
 # Tools 
-sudo pacman -S --noconfirm --needed xclip gcolor3
+sudo pacman -S --noconfirm --needed xclip gcolor3  flameshot firefox
  # Archiver
 sudo pacman -S --noconfirm --needed pcmanfm xarchiver unrar unzip p7zip zip zathura zathura-pdf-mupdf ntfs-3g exfat-utils   
 # Additional
-sudo pacman -S --noconfirm --needed rofi libnotify dunst neofetch acpi htop usbutils lxsession jdk-openjdk nodejs npm android-tools
+sudo pacman -S --noconfirm --needed rofi dunst libnotify neofetch acpi htop usbutils lxsession jdk-openjdk nodejs npm android-tools
 # Terminal
-sudo pacman -S --noconfirm --needed starship exa bat kitty bash-completion zsh zsh-history-substring-search zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -S --noconfirm --needed starship exa bat kitty zsh zsh-history-substring-search zsh-autosuggestions zsh-syntax-highlighting
 # Functionality
 sudo pacman -S  --noconfirm --needed exfat-utils  fuse gvfs gvfs-mtp
 sudo pacman -S --noconfirm --needed qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
-sudo pacman -S ebtables iptables
-sudo useradd -G libvirt linus 
-newgrp libvirt
-sudo systemctl enable --now virtqemud
-sudo systemctl enable --now virtstoraged
-sudo systemctl enable --now virtnetworkd
+sudo pacman -S  iptables-nft
+sudo usermod -a -G libvirt linus 
+#sudo systemctl enable --now virtqemud
+#sudo systemctl enable --now virtstoraged
+#sudo systemctl enable --now virtnetworkd
 echo "exec i3" > .xinitrc
+echo "Chaning login shell"
+sudo usermod --shell /bin/zsh linus
                   

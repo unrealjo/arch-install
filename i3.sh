8e0f89fa57372c0d0d85e9338090af5af8640e93
Youssef Eddaif <yeddaif18@gmail.com>
2022-01-20 21:21:12 +0100

    Organizing list of packages

--- i3.sh
+++ i3.sh
@@ -1,24 +1,32 @@
- #!/bin/bash
- # Xorg
-pacman -S --noconfirm --needed linux-firmware base-devel rust xorg-server xorg-xinit xorg-xrandr xorg-xkill intel-ucode 
- # Audio and video
-pacman -S --noconfirm --needed  pulseaudio mpv mpd mpc brightnessctl lxappearance i3-gaps feh pamixer
-# Tools 
-pacman -S --noconfirm --needed xclip gcolor3  flameshot 
- # Archiver
-pacman -S --noconfirm --needed pcmanfm xarchiver unrar unzip p7zip zip zathura zathura-pdf-mupdf ntfs-3g exfat-utils   
+#!/bin/bash
+# Xorg
+pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-xrandr xorg-xkill  # Dispaly Server
+# Building 
+pacman -S --noconfirm --needed base-devel rust # For AUR
+# Development
+pacman -S --noconfirm --needed nodejs npm android-tools git 
+# Firmware
+pacman -S --noconfirm --needed linux-firmware intel-ucode 
+# Newtork
+pacman -S --noconfirm --needed iptables-nft dnsmasq bridge-utils openbsd-netcat # Most of the needed for libvirt 
+# Audio and video
+pacman -S --noconfirm --needed pulseaudio mpv mpd mpc brightnessctl pamixer
+# Wm
+pacman -S --noconfirm --needed i3-gaps i3lock
+# Appearance 
+pacman -S --noconfirm --needed feh lxappearance
+# Tools
+pacman -S --noconfirm --needed xclip gcolor3 flameshot alacritty kitty # Good to have 2 terminals :)
+# Devices and disk Management
+pacman -S --noconfirm --needed exfat-utils fuse gvfs gvfs-mtp ntfs-3g vde2 acpi usbutils 
+# Archiver
+pacman -S --noconfirm --needed pcmanfm xarchiver unrar unzip p7zip zip zathura zathura-pdf-mupdf  
 # Additional
-pacman -S --noconfirm --needed rofi dunst libnotify neofetch acpi htop usbutils lxsession nodejs npm android-tools
-# Terminal
-pacman -S --noconfirm --needed starship exa bat alacritty fish mcfly
-# Functionality
-pacman -S  --noconfirm --needed exfat-utils  fuse gvfs gvfs-mtp
-pacman -S --noconfirm --needed qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
-#pacman -S  iptables-nft
+pacman -S --noconfirm --needed rofi dunst libnotify lxsession
+# CLI
+pacman -S --noconfirm --needed neofetch htop starship exa bat fish mcfly neovim
+# Virtualisation
+pacman -S --noconfirm --needed qemu virt-manager virt-viewer
+#pacman -S  
 usermod -a -G libvirt linus 
-systemctl enable --now virtqemud
-systemctl enable --now virtstoraged
-systemctl enable --now virtnetworkd
-echo "Chaning login shell"
-usermod --shell /bin/fish linus
-                  
+

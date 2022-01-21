7eec2b9c0bfc498a6e42f8d41dbf29029d8d2aac
Youssef Eddaif <yeddaif18@gmail.com>
2022-01-21 16:47:22 +0100

    Resync better mirrors

--- base.sh
+++ base.sh
@@ -16,6 +16,13 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 Arch.localdomain Arch" >> /etc/hosts
 echo "STEP ON DONE !"
+#Add parallel downloading
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+pacman -S --noconfirm pacman-contrib curl
+pacman -S --noconfirm reflector rsync 
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
+
 echo "installing essential packages"
 pacman -S --noconfirm grub iw iwd dhcpcd sudo
 systemctl enable iwd dhcpcd

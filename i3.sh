5d5e53633d156af699ce97711a3f13fb487b0817
Youssef Eddaif <yeddaif18@gmail.com>
2022-02-01 10:42:43 +0100

    Removing unused backages anymore

--- i3.sh
+++ i3.sh
@@ -2,13 +2,13 @@
 # Xorg
 pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-xrandr xorg-xkill  # Dispaly Server
 # Building 
-pacman -S --noconfirm --needed base-devel rust # For AUR
+pacman -S --noconfirm --needed base-devel
 # Development
 pacman -S --noconfirm --needed nodejs npm android-tools git 
 # Firmware
-pacman -S --noconfirm --needed linux-firmware intel-ucode 
+pacman -S --noconfirm --needed intel-ucode 
 # Newtork
-pacman -S --needed iptables-nft dnsmasq bridge-utils openbsd-netcat # Most of the needed for libvirt 
+pacman -S --needed iptables-nft dnsmasq bridge-utils openbsd-netcat # Most of them needed for libvirt 
 # Audio and video
 pacman -S --noconfirm --needed pulseaudio mpv mpd mpc brightnessctl pamixer
 # Wm
@@ -16,13 +16,13 @@ pacman -S --noconfirm --needed i3-gaps i3lock
 # Appearance 
 pacman -S --noconfirm --needed feh lxappearance
 # Tools
-pacman -S --noconfirm --needed xclip gcolor3 flameshot alacritty kitty # Good to have 2 terminals :)
+pacman -S --noconfirm --needed xclip gcolor3 flameshot alacritty
 # Devices and disk Management
 pacman -S --noconfirm --needed exfat-utils fuse gvfs gvfs-mtp ntfs-3g vde2 acpi usbutils 
 # Archiver
 pacman -S --noconfirm --needed pcmanfm xarchiver unrar unzip p7zip zip zathura zathura-pdf-mupdf  
 # Additional
-pacman -S --noconfirm --needed rofi dunst libnotify lxsession
+pacman -S --noconfirm --needed rofi dunst libnotify lxsession  firejail
 # CLI
 pacman -S --noconfirm --needed neofetch htop starship exa bat fish mcfly neovim
 # Virtualisation

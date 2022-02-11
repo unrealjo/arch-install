88607d6c0d524a5e8e383091536de636fc56cace
Youssef Eddaif <yeddaif18@gmail.com>
2022-02-11 13:01:41 +0100

    mpv -> vlc , bye zathura (pkg broke) , and more changes

--- i3.sh
+++ i3.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
+# Newtork
+pacman -S --needed iptables-nft dnsmasq bridge-utils openbsd-netcat # Most of them needed for libvirt 
 # Xorg
 pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-xrandr xorg-xkill  # Dispaly Server
-# Building 
-pacman -S --noconfirm --needed base-devel
 # Development
 pacman -S --noconfirm --needed nodejs npm 
 # Firmware
 pacman -S --noconfirm --needed intel-ucode 
-# Newtork
-pacman -S --needed iptables-nft dnsmasq bridge-utils openbsd-netcat # Most of them needed for libvirt 
 # Audio and video
-pacman -S --noconfirm --needed pulseaudio mpv mpd mpc brightnessctl pamixer
+pacman -S --noconfirm --needed pulseaudio vlc  mpd mpc brightnessctl pamixer # Bye mpv , welcome vlc
 # Wm
-pacman -S --noconfirm --needed i3-gaps i3lock
+pacman -S --noconfirm --needed i3-gaps i3loack spectrwm tint2 # i3 just in case
 # Appearance 
-pacman -S --noconfirm --needed feh lxappearance
+pacman -S --noconfirm --needed feh lxappearance kvantum qt5ct  # qt and gtk theming 
 # Tools
-pacman -S --noconfirm --needed xclip gcolor3 flameshot alacritty
+pacman -S --noconfirm --needed xclip gcolor3 flameshot alacritty syncthing network-manager-applet
 # Devices and disk Management
-pacman -S --noconfirm --needed exfat-utils fuse gvfs gvfs-mtp ntfs-3g vde2 acpi usbutils 
+pacman -S --noconfirm --needed exfat-utils fuse gvfs gvfs-mtp ntfs-3g vde2 acpi usbutils xdg-user-dirs
 # Archiver
-pacman -S --noconfirm --needed pcmanfm xarchiver unrar unzip p7zip zip zathura zathura-pdf-mupdf  
+pacman -S --noconfirm --needed pcmanfm xarchiver unrar unzip p7zip zip  
 # Additional
 pacman -S --noconfirm --needed rofi dunst libnotify lxsession  firejail
 # CLI
-pacman -S --noconfirm --needed neofetch htop starship exa bat fish mcfly neovim
+pacman -S --noconfirm --needed neofetch htop starship exa bat  neovim fzf
+# Zsh
+pacman -S --noconfirm --needed zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting zsh-theme-powerlevel10k
 # Virtualisation
 pacman -S --noconfirm --needed qemu virt-manager virt-viewer
 #pacman -S  
